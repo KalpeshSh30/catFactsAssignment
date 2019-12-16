27e426dbb24d24586f4ce2975bf6c5fa00853762
Kalpesh Shirudkar <kalpeshshirudkar@gmail.com>
2019-12-16 13:20:51 +0530

    Fixed code and removed all problem from first commit

--- index.js
+++ index.js
@@ -3,39 +3,37 @@ const axios = require('axios');
 const URL = 'https://cat-fact.herokuapp.com/facts';
 
 async function getCatsFacts(){
-
-    const response = await axios.get(URL);
-    const data = await response.data;
-    
-    return data; 
+    try{
+        const response = await axios.get(URL);
+        const data = response.data;
+        return data;
+    }
+    catch(error){
+        console.log(error.code);
+    } 
 }
 
-function returnFilteredAndUpdatedData(data){
-    let updatedData = data.all.map((ele) => {
+function returnUpdatedData(data){
+    const updatedData = data.all.map((ele) => {
             if(ele.user){
                 ele.user['fullName'] = `${ele.user.name.first}  ${ele.user.name.last}`;
                 return ele;
             }
     });
-    console.log(updatedData[0]);
-    filterDataWithLessUpvotes(updatedData);
+    return updatedData;
 }
 
 function filterDataWithLessUpvotes(data){
-    let dataWithUpvotesGreaterThanTwo = [];
-     dataWithUpvotesGreaterThanTwo = data.filter((ele) => {
+     const dataWithUpvotesGreaterThanTwo = data.filter((ele) => {
         if(ele)
             return ele.upvotes >= 2;
     });
-
     for(let factsAndUpvotes of dataWithUpvotesGreaterThanTwo){
         console.log(`Fact : ${factsAndUpvotes.text} \nUpvotes : ${factsAndUpvotes.upvotes} \n\n\n`);
     }
 }
-
-const data = getCatsFacts()
+getCatsFacts()
     .then((data) => {
-        //console.log(data.all[0]);
-        returnFilteredAndUpdatedData(data);
+        const updatedData = returnUpdatedData(data);
+        filterDataWithLessUpvotes(updatedData);
     });
-
